3b2ec92046458a91bf7202fbda67706e7a5d7b94
Ronny2411 <rohitrastogi576@gmail.com>
2024-02-01 17:10:21 +0530

    Final Commit

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -3,7 +3,7 @@ package com.example.newsapp.util
 object Constants {
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
-    const val API_KEY = "add_your_api_key"
+    const val API_KEY = "add_your_api_key_here"
     const val BASE_URL = "https://newsapi.org/v2/"
     const val NEWS_DB_NAME = "news_db"
 }
